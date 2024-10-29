1b245bc6fa638989cdf71466d62bb952a085e1cf
DaltonSacramento <dalton.sacramento@sou.unifal-mg.edu.br>
2024-10-29 11:32:53 -0300

    arvorepartilha

--- AEDS2-T/VScodeProjects/Implementações-Aulas/AULA16-04do10/partilha.c
+++ AEDS2-T/VScodeProjects/Implementações-Aulas/AULA16-04do10/partilha.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 typedef struct node_{
-    int chave;
+    int valor;
     int partilha;
     struct node_ *esq;
     struct node_ *dir;
+    int freq;
 }node;
 
 typedef struct vet_{
@@ -13,51 +14,79 @@ typedef struct vet_{
     int freq;
 }vet;
 
-node *construirss(vet V[], int m, int n){
-    node *no=malloc(sizeof(node));
-    if(m==n){
-        return NULL;
-    }else{
-        int y, s;
-        y=V[m].freq;
-        s=m/2;
-        no->chave=y;
-        no->partilha=s;
-        no->esq=construirss(V,s,n);
-        no->dir=construirss(V,n,s);
+// node *construirss(vet V[], int m, int n){
+//     node *no=malloc(sizeof(node));
+//     if(m==n){
+//         return NULL;
+//     }else{
+//         int y, s;
+//         y=V[m].freq;
+//         s=m/2;
+//         no->chave=y;
+//         no->partilha=s;
+//         no->esq=construirss(V,s,n);
+//         no->dir=construirss(V,n,s);
+//     }
+//     return no;
+// }
+
+// node *construir(vet V[], int m, int n) {
+//     // Verifica se o intervalo é válido
+//     if (m > n) {
+//         return NULL;
+//     }
+
+//     // Calcula o ponto de partição (meio do intervalo)
+//     int s = (m + n) / 2;
+
+//     // Aloca um novo nó
+//     node *no = (node *)malloc(sizeof(node));
+
+//     // Inicializa o nó:
+//     no->chave = V[s].chave;      // A chave do nó é a chave do vetor na posição s
+//     no->partilha = V[((m+n)-1)/2].chave;   // O campo partilha agora armazena a chave do elemento mediano
+
+//     // Chama recursivamente para construir as subárvores
+//     no->esq = construir(V, m, s - 1); // Subárvore esquerda: elementos menores que a partição
+//     no->dir = construir(V, s + 1, n); // Subárvore direita: elementos maiores que a partição
+//     return no;
+// }
+
+// // Função para imprimir a árvore (pré-ordem)
+// void imprimir_arvore(node *raiz) {
+//     if (raiz != NULL) {
+//         printf("Chave: %d, Partilha (chave mediana): %d\n", raiz->chave, raiz->partilha);
+//         imprimir_arvore(raiz->esq);
+//         imprimir_arvore(raiz->dir);
+//     }
+// }
+
+node* construçao(node v[], int inf, int sup){
+    node* raiz=malloc(sizeof(node));
+    if(inf!=sup){
+        int max=inf;
+        for(int i=inf;i<=sup;i++){
+            if(v[i].freq>v[max].freq){
+                max=i;
+            }
+        }
+        raiz->valor=v[max].valor;
+        for(int i=max;i<sup;i++){
+            v[i]=v[i+1];
+        }
+        sup--;
+        int mid=(inf+sup)/2;
+        raiz->partilha=v[mid].valor;
+        raiz->esq=construçao(v,inf,mid);
+        raiz->dir=construçao(v,mid+1,sup);
     }
-    return no;
-}
-
-node *construir(vet V[], int m, int n) {
-    // Verifica se o intervalo é válido
-    if (m > n) {
-        return NULL;
+    else{
+        raiz->valor=v[inf].valor;
+        raiz->partilha=-1;
+        raiz->esq=NULL;
+        raiz->dir=NULL;
     }
-
-    // Calcula o ponto de partição (meio do intervalo)
-    int s = (m + n) / 2;
-
-    // Aloca um novo nó
-    node *no = (node *)malloc(sizeof(node));
-
-    // Inicializa o nó:
-    no->chave = V[s].chave;      // A chave do nó é a chave do vetor na posição s
-    no->partilha = V[((m+n)-1)/2].chave;   // O campo partilha agora armazena a chave do elemento mediano
-
-    // Chama recursivamente para construir as subárvores
-    no->esq = construir(V, m, s - 1); // Subárvore esquerda: elementos menores que a partição
-    no->dir = construir(V, s + 1, n); // Subárvore direita: elementos maiores que a partição
-    return no;
-}
-
-// Função para imprimir a árvore (pré-ordem)
-void imprimir_arvore(node *raiz) {
-    if (raiz != NULL) {
-        printf("Chave: %d, Partilha (chave mediana): %d\n", raiz->chave, raiz->partilha);
-        imprimir_arvore(raiz->esq);
-        imprimir_arvore(raiz->dir);
-    }
+    return raiz;
 }
 
 int main() {
