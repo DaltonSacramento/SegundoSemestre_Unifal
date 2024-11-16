bc2945c76bdcb94513e3c6525b18d7bc19853236
DaltonSacramento <dalton.sacramento@sou.unifal-mg.edu.br>
2024-11-16 14:31:54 -0300

    Alteraçao na main

--- AEDS2-Prática/Trabalhos/Tp-3/ordenaçoes.c
+++ AEDS2-Prática/Trabalhos/Tp-3/ordenaçoes.c
@@ -4,38 +4,43 @@
 
 //#define MAX_STRING 100
 
-int main(){
-
-    ///////////////////////////////////////////////////////////
-    /////////////////// Leitor de instâncias //////////////////
-    ///////////////// Não deve ser modificado /////////////////
-    ///////////////////////////////////////////////////////////
-    //int instancia_num = -1;
-
-    // instancia_num = atoi(argv[1]);
-    // if (instancia_num <= 0 || instancia_num > 10) {
-    //     printf("Para evalecutar o código, digite ./arvores val\nonde val é um número entre 1 e 10 que simboliza a instância utilizada\n");
-    //     return(0);
-    // }
+int main(int argc, char* argv[]){
 
     char caminho[256];// Buffer para armazenar o caminho completo do arquivo
     // Constrói o caminho para o arquivo dentro da pasta "instancias"
     snprintf(caminho, sizeof(caminho), "jogadores/jogador.csv");
 
-    FILE* pontarq = fopen(caminho, "r");
-
-    if (pontarq == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        return(1);
+    int instancia_num = atoi(argv[1]);
+    if (instancia_num==1){
+        FILE* pontarq = fopen(caminho, "r");
+        if (pontarq == NULL) {
+            printf("Erro ao abrir o arquivo.\n");
+            return(1);
+        }
+        double tempo_ord_simples= ord_simples(pontarq);
+        fclose(pontarq);//Fecha o arquivo de texto
+        printf("Tempo: %f", tempo_ord_simples);
+    }
+    if (instancia_num==2){
+        FILE* pontarq = fopen(caminho, "r");
+        if (pontarq == NULL) {
+            printf("Erro ao abrir o arquivo.\n");
+            return(1);
+        }
+        double tempo_ord_otima = ord_otima(pontarq);
+        fclose(pontarq);//Fecha o arquivo de texto
+    }
+    if (instancia_num==3){
+        FILE* pontarq = fopen(caminho, "r");
+        if (pontarq == NULL) {
+            printf("Erro ao abrir o arquivo.\n");
+            return(1);
+        }
+        double tempo_ord_tempolinear = ord_tempolinear(pontarq);
+        fclose(pontarq);//Fecha o arquivo de texto
     }
 
-    double tempo_ord_simples;
-    tempo_ord_simples = ord_simples(pontarq);
-    // double tempo_ord_otima = ord_otima(instancia_num);
-    // double tempo_ord_linear = ord_tempolinear(instancia_num);
-    fclose(pontarq);//Fecha o arquivo de texto
-
-    printf("Tempo: %f", tempo_ord_simples);
+    
 
     return 0;
 }
